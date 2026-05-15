994e419d126be46a04a441ff4bfb3473c93ff641
canalesmejiad <david.kanale@hotmail.es>
2026-05-14 21:07:53 -0700

    cm

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
+import { testConnection } from './src/models/db.js'
+
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
 const PORT = process.env.PORT || 3000;
 
@@ -40,7 +42,12 @@ app.get('/categories', async (req, res) => {
     res.render('categories', { title: 'Categories' });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://127.0.0.1:${PORT}`);
-    console.log(`Environment: ${NODE_ENV}`);
-});
+app.listen(PORT, async () => {
+    try {
+        await testConnection()
+        console.log(`Server is running at http://127.0.0.1:${PORT}`)
+        console.log(`Environment: ${NODE_ENV}`)
+    } catch (error) {
+        console.error("Error connecting to the database:", error)
+    }
+})
