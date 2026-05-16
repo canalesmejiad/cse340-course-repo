660ea05d1ab624a72da90826c7499851daad2d04
canalesmejiad <david.kanale@hotmail.es>
2026-05-16 09:24:02 -0700

    Completed categories and projects features

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import path from 'path';
 import { testConnection } from './src/models/db.js'
 import { getAllOrganizations } from './src/models/organizations.js';
 import { getAllProjects } from './src/models/projects.js';
+import { getAllCategories } from './src/models/categories.js';
 
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
 const PORT = process.env.PORT || 3000;
@@ -49,7 +50,11 @@ app.get('/projects', async (req, res) => {
 });
 
 app.get('/categories', async (req, res) => {
-    res.render('categories', { title: 'Categories' });
+    const categories = await getAllCategories();
+    console.log(categories);
+
+    const title = 'Service Categories';
+    res.render('categories', { title, categories });
 });
 
 app.listen(PORT, async () => {
