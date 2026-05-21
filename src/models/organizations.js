4383b6968f1fda3aacbca0663c28c7623b090b8a
canalesmejiad <david.kanale@hotmail.es>
2026-05-20 20:42:56 -0700

    cm

--- src/models/organizations.js
+++ src/models/organizations.js
@@ -1,6 +1,6 @@
 import db from './db.js'
 
-const getAllOrganizations = async() => {
+const getAllOrganizations = async () => {
     const query = `
         SELECT organization_id, name, description, contact_email, logo_filename
         FROM public.organization;
@@ -10,5 +10,24 @@ const getAllOrganizations = async() => {
 
     return result.rows;
 }
+const getProjectsByOrganizationId = async (organizationId) => {
+    const query = `
+        SELECT
+            project_id,
+            organization_id,
+            title,
+            description,
+            location,
+            date
+        FROM project
+        WHERE organization_id = $1
+        ORDER BY date;
+    `;
 
-export { getAllOrganizations }
+    const queryParams = [organizationId];
+    const result = await db.query(query, queryParams);
+
+    return result.rows;
+};
+
+export { getAllProjects, getProjectsByOrganizationId };
